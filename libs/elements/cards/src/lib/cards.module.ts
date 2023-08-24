fbb0e8b89bd6b28e03fb76936fda9450b8fd5583
dancunrandy <dancunrandy@gmail.com>
2023-08-24 08:34:44 +0300

    imported button module to cards module

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -1,9 +1,10 @@
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaInfoCardComponent } from './elewa-info-card/elewa-info-card.component';
 import { ElewaWebsitePriceItemCardComponent } from './elewa-website-price-item-card/elewa-website-price-item-card.component';
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule,ButtonsModule],
   declarations: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent],
   exports: [ElewaInfoCardComponent]
 })
